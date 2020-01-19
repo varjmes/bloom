b3c80b639f222b3c92e420bd9a65cfd594aa4390
James Spencer <hello@jmes.tech>
2020-01-19 10:42:26 +0000

    chore: normalize.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,6 @@ import { AppContainer, AppHeader, AppTitle, AppIntro } from './styles'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
